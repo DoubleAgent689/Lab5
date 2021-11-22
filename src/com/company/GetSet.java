19202134e1e810429ad6f52f4c9ca63fd2d880e0
DoubleAgent689 <art567dead43@gmail.com>
2021-11-22 09:51:18 +0400

    Initial commit

---
+++ src/com/company/GetSet.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import java.io.OutputStream;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public interface GetSet {
+    public int[] getArray();
+}
